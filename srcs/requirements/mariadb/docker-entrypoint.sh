7cd06ce0519ded814c99086c3eb781233f4eff1a
fli <fli@student.42.fr>
2024-12-19 18:10:51 +0100

    same

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,80 +1,80 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # if [ -f .env ]; then
-# #     source .env
-# # else
-# #     echo ".env file not found! $PWD"
-# #     exit 1
-# # fi
-
-# # chmod -R 755 /var/lib/mysql
-
-# set -e
-
-# chown -R mysql:mysql /var/lib/mysql
-
-# mkdir -p /run/mysqld
-
-# chown -R mysql:mysql /run/mysqld
-
-# # service mysql start;
-
-# echo "Not HERE"
-
-# if ! mysqladmin ping --silent; then
-#   mysqld --user=mysql &
-#   sleep 10
+# if [ -f .env ]; then
+#     source .env
+# else
+#     echo ".env file not found! $PWD"
+#     exit 1
 # fi
 
-# echo "Here !!!"
-# # until mariadb -e "SELECT 1;" >/dev/null 2>&1; do
-# #     echo "Waiting for MariaDB to start..."
-# #     sleep 2
-# # done
+# chmod -R 755 /var/lib/mysql
 
-# # until mysqladmin ping --silent --socket=/run/mysqld/mysqld.sock; do
-# #     echo "Waiting for MariaDB to start..."
-# #     sleep 2
-# # done
+set -e
 
-# # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+chown -R mysql:mysql /var/lib/mysql
 
-# # mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mkdir -p /run/mysqld
 
-# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+chown -R mysql:mysql /run/mysqld
 
-# # mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# service mysql start;
 
-# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+echo "Not HERE"
 
-# # mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+if ! mysqladmin ping --silent; then
+  mysqld --user=mysql &
+  sleep 10
+fi
 
-# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-# # mysql -e "FLUSH PRIVILEGES;"
-
-# # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-# mysqld_safe --datadir=/var/lib/mysql &
-
-# until mysqladmin ping -uroot -p"${SQL_ROOT_PASSWORD}" --silent; do
-#   echo "Waiting DB to be ready"
-#   sleep 5
+echo "Here !!!"
+# until mariadb -e "SELECT 1;" >/dev/null 2>&1; do
+#     echo "Waiting for MariaDB to start..."
+#     sleep 2
 # done
 
-# mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# until mysqladmin ping --silent --socket=/run/mysqld/mysqld.sock; do
+#     echo "Waiting for MariaDB to start..."
+#     sleep 2
+# done
 
-# mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# mysql -u root -e "FLUSH PRIVILEGES;"
+# mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# exec mysqld --user=mysql
-# # wait
+# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# mysql -e "FLUSH PRIVILEGES;"
+
 # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-# # exec mysqld_safe
 
-# # mysqld
+mysqld_safe --datadir=/var/lib/mysql &
+
+until mysqladmin ping -uroot -p"${SQL_ROOT_PASSWORD}" --silent; do
+  echo "Waiting DB to be ready"
+  sleep 5
+done
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+exec mysqld --user=mysql
+# wait
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
+
+# mysqld
