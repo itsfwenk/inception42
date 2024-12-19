b3deda102a5a0110d1b480f172561e444b3b4b66
fli <fli@student.42.fr>
2024-12-19 16:44:32 +0100

    mariadb UGHHHHHHHHHHHHH

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,63 +1,80 @@
-#!/bin/bash
+# #!/bin/bash
 
-# if [ -f .env ]; then
-#     source .env
-# else
-#     echo ".env file not found! $PWD"
-#     exit 1
+# # if [ -f .env ]; then
+# #     source .env
+# # else
+# #     echo ".env file not found! $PWD"
+# #     exit 1
+# # fi
+
+# # chmod -R 755 /var/lib/mysql
+
+# set -e
+
+# chown -R mysql:mysql /var/lib/mysql
+
+# mkdir -p /run/mysqld
+
+# chown -R mysql:mysql /run/mysqld
+
+# # service mysql start;
+
+# echo "Not HERE"
+
+# if ! mysqladmin ping --silent; then
+#   mysqld --user=mysql &
+#   sleep 10
 # fi
 
-# chmod -R 755 /var/lib/mysql
+# echo "Here !!!"
+# # until mariadb -e "SELECT 1;" >/dev/null 2>&1; do
+# #     echo "Waiting for MariaDB to start..."
+# #     sleep 2
+# # done
 
-# service mysql start;
+# # until mysqladmin ping --silent --socket=/run/mysqld/mysqld.sock; do
+# #     echo "Waiting for MariaDB to start..."
+# #     sleep 2
+# # done
 
-RUN chown -R mysql:mysql /var/lib/mysql
+# # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-if ! mysqladmin ping --silent; then
-  mysqld --user=mysql &
-  sleep 10
-fi
+# # mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-until mysqladmin ping --silent; do
-    echo "Waiting for MariaDB to start..."
-    sleep 2
-done
+# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# # mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# # mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# # mysql -e "FLUSH PRIVILEGES;"
 
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysqld_safe --datadir=/var/lib/mysql &
 
-# mysql -e "FLUSH PRIVILEGES;"
+# until mysqladmin ping -uroot -p"${SQL_ROOT_PASSWORD}" --silent; do
+#   echo "Waiting DB to be ready"
+#   sleep 5
+# done
 
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
+
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# mysql -u root -e "FLUSH PRIVILEGES;"
+
+# exec mysqld --user=mysql
+# # wait
 # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# # exec mysqld_safe
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
-
-# Set root password
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-# Flush privileges
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-
-exec mysqld --user=mysql
-# wait
-
-# exec mysqld_safe
-
-# mysqld
+# # mysqld
