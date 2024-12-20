4a0bd77077e5f025fb2bc00a3e732b5d7b639d29
fli <fli@student.42.fr>
2024-12-20 15:40:59 +0100

    kinda working

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -7,74 +7,85 @@
 #     exit 1
 # fi
 
-# chmod -R 755 /var/lib/mysql
+# # chmod -R 755 /var/lib/mysql
 
-set -e
+# set -e
 
-chown -R mysql:mysql /var/lib/mysql
+# chown -R mysql:mysql /var/lib/mysql
 
-mkdir -p /run/mysqld
+# mkdir -p /run/mysqld
 
-chown -R mysql:mysql /run/mysqld
+# chown -R mysql:mysql /run/mysqld
 
-# service mysql start;
+# # service mysql start;
 
-echo "Not HERE"
+# if ! mysqladmin ping --silent; then
+#   mysqld --user=mysql &
+#   sleep 10
+# fi
 
-if ! mysqladmin ping --silent; then
-  mysqld --user=mysql &
-  sleep 10
-fi
+# # until mariadb -e "SELECT 1;" >/dev/null 2>&1; do
+# #     echo "Waiting for MariaDB to start..."
+# #     sleep 2
+# # done
 
-echo "Here !!!"
-# until mariadb -e "SELECT 1;" >/dev/null 2>&1; do
-#     echo "Waiting for MariaDB to start..."
-#     sleep 2
+# # until mysqladmin ping --silent --socket=/run/mysqld/mysqld.sock; do
+# #     echo "Waiting for MariaDB to start..."
+# #     sleep 2
+# # done
+
+# # mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# # mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# # mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# # mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# # mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# # mysql -e "FLUSH PRIVILEGES;"
+
+# # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# mysqld_safe --datadir=/var/lib/mysql &
+
+# until mysqladmin ping -uroot -p"${SQL_ROOT_PASSWORD}" --silent; do
+#   echo "Waiting DB to be ready"
+#   sleep 5
 # done
 
-# until mysqladmin ping --silent --socket=/run/mysqld/mysqld.sock; do
-#     echo "Waiting for MariaDB to start..."
-#     sleep 2
-# done
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
 
-# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-# mysql -e "CREATE USER '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
 
-# mysql -u "${SQL_USER}" -p"${SQL_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-# mysql -e "FLUSH PRIVILEGES;"
-
+# exec mysqld --user=mysql
+# # wait
 # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# # exec mysqld_safe
 
-mysqld_safe --datadir=/var/lib/mysql &
+# # mysqld
 
-until mysqladmin ping -uroot -p"${SQL_ROOT_PASSWORD}" --silent; do
-  echo "Waiting DB to be ready"
-  sleep 5
-done
+set -euo pipefail
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+service mariadb start
+sleep 3
 
-mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rootpassword';"
+mysql -e "FLUSH PRIVILEGES;"
 
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
-
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-exec mysqld --user=mysql
-# wait
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-# exec mysqld_safe
-
-# mysqld
+mysqladmin -u root shutdown
+exec mysqld
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -1,54 +1,94 @@
 #!/bin/bash
 
-POOL_CONF="/etc/php/7.4/fpm/pool.d/www.conf"
+#Make sure MariaDB has started properly
+sleep 10
 
-if grep -q "^clear_env" "$POOL_CONF"; then
-    echo "Updating 'clear_env' directive..."
-    sed -i 's/^clear_env.*/clear_env = no/' "$POOL_CONF"
+if [ -f "/var/www/html/wp-config.php" ]; then
+	echo "wp-config file already set up."
 else
-    echo "Adding 'clear_env = no' directive..."
-    echo "clear_env = no" >> "$POOL_CONF"
+	if [ ! -d "/var/www/html/wp-content" ]; then
+		wp core download --allow-root || { echo "Wordpress download failed"; exit 1; }
+	else
+		echo "WordPress files already present, skipping download."
+	fi
+	echo "Setting up wp-config.php..."
+	wp core config --allow-root \
+	       	--dbname="${SQL_DATABASE}" \
+		--dbuser="${SQL_USER}" \
+		--dbpass="${SQL_PASSWORD}" \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/html' || { echo "Wordpress core config failed"; exit 1; }
+
+	echo "Installing WordPress..."
+	wp core install \
+		--url="${WORDPRESS_URL}" \
+		--title="${WORDPRESS_TITLE}" \
+		--admin_user="${WORDPRESS_ADMIN_USER}" \
+		--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+		--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+		--allow-root || { echo "Wordpress core install failed"; exit 1; }
+
+	echo "Creating user..."
+	wp user create "${USER_USERNAME}" "${USER_EMAIL}" \
+		--role=subscriber \
+		--user_pass="${USER_PASSWORD}" \
+		--allow-root|| { echo "Wordpress user create failed"; exit 1; }
 fi
 
-if grep -q "^listen" "$POOL_CONF"; then
-    echo "Updating 'listen' directive..."
-    sed -i 's/^listen.*/listen = wordpress:9000/' "$POOL_CONF"
-else
-    echo "Adding 'listen = wordpress:9000' directive..."
-    echo "listen = wordpress:9000" >> "$POOL_CONF"
-fi
+exec php-fpm7.4 -F -R
 
-# echo "Restarting PHP-FPM service..."
-# systemctl restart php7.4-fpm
-# docker exec wordpress pkill -USR2 php-fpm
 
-echo "PHP configuration changes applied successfully!"
+# #!/bin/bash
 
-until mysql -h db -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1" &> /dev/null; do
-    echo "Waiting for the database to be ready..."
-    sleep 3
-done
+# # POOL_CONF="/etc/php/7.4/fpm/pool.d/www.conf"
 
-# if [ ! -f /var/www/wordpress/wp-config.php ]; then
-#     cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-#     sed -i "s/database_name_here/wordpress/" /var/www/wordpress/wp-config.php
-#     sed -i "s/username_here/fli/" /var/www/wordpress/wp-config.php
-#     sed -i "s/password_here/wordpress_password/" /var/www/wordpress/wp-config.php
-#     sed -i "s/localhost/db/" /var/www/wordpress/wp-config.php
-# fi
+# # if grep -q "^clear_env" "$POOL_CONF"; then
+# #     echo "Updating 'clear_env' directive..."
+# #     sed -i 's/^clear_env.*/clear_env = no/' "$POOL_CONF"
+# # else
+# #     echo "Adding 'clear_env = no' directive..."
+# #     echo "clear_env = no" >> "$POOL_CONF"
+# # fi
 
-wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+# # if grep -q "^listen" "$POOL_CONF"; then
+# #     echo "Updating 'listen' directive..."
+# #     sed -i 's/^listen.*/listen = wordpress:9000/' "$POOL_CONF"
+# # else
+# #     echo "Adding 'listen = wordpress:9000' directive..."
+# #     echo "listen = wordpress:9000" >> "$POOL_CONF"
+# # fi
 
-sudo -u www-data wp core install --url='fli.42.fr' \
-								--title='Inception' \
-								--admin_user=$WP_ADMINUSER \
-								--admin_password=$WP_ADMINPW \
-								--admin_email='fli@student.42.fr'
+# # echo "Restarting PHP-FPM service..."
+# # systemctl restart php7.4-fpm
+# # docker exec wordpress pkill -USR2 php-fpm
 
-wp user create fliuser fli@user.fr
+# # echo "PHP configuration changes applied successfully!"
 
-php-fpm
+# until mysql -h db -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1" &> /dev/null; do
+#     echo "Waiting for the database to be ready..."
+#     sleep 3
+# done
+
+# # if [ ! -f /var/www/wordpress/wp-config.php ]; then
+# #     cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+# #     sed -i "s/database_name_here/wordpress/" /var/www/wordpress/wp-config.php
+# #     sed -i "s/username_here/fli/" /var/www/wordpress/wp-config.php
+# #     sed -i "s/password_here/wordpress_password/" /var/www/wordpress/wp-config.php
+# #     sed -i "s/localhost/db/" /var/www/wordpress/wp-config.php
+# # fi
+
+# wp config create	--allow-root \
+# 					--dbname=$SQL_DATABASE \
+# 					--dbuser=$SQL_USER \
+# 					--dbpass=$SQL_PASSWORD \
+# 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+# sudo -u www-data wp core install --url='fli.42.fr' \
+# 								--title='Inception' \
+# 								--admin_user=$WP_ADMINUSER \
+# 								--admin_password=$WP_ADMINPW \
+# 								--admin_email='fli@student.42.fr'
+
+# wp user create fliuser fli@user.fr
+
+# php-fpm
