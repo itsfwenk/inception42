6a6347ecf52f84357321c12af6e1461870195899
fli <fli@student.42.fr>
2024-12-23 17:50:24 +0100

    mandatory working :o

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -79,13 +79,19 @@
 set -euo pipefail
 
 service mariadb start
+
 sleep 3
 
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+
 mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rootpassword';"
+
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${ROOT_PASSWORD}';"
+
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
+
 exec mysqld
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-#Make sure MariaDB has started properly
 sleep 10
 
 if [ -f "/var/www/html/wp-config.php" ]; then
-	echo "wp-config file already set up."
+	echo "Skipping wp-config file set up."
 else
 	if [ ! -d "/var/www/html/wp-content" ]; then
-		wp core download --allow-root || { echo "Wordpress download failed"; exit 1; }
+		wp core download --allow-root || { echo "Wordpress download failure"; exit 1; }
 	else
 		echo "WordPress files already present, skipping download."
 	fi
-	echo "Setting up wp-config.php..."
+	echo "Setting wp-config.php file"
 	wp core config --allow-root \
-	       	--dbname="${SQL_DATABASE}" \
-		--dbuser="${SQL_USER}" \
-		--dbpass="${SQL_PASSWORD}" \
-		--dbhost=mariadb:3306 \
-		--path='/var/www/html' || { echo "Wordpress core config failed"; exit 1; }
+					--dbname="${SQL_DATABASE}" \
+					--dbuser="${SQL_USER}" \
+					--dbpass="${SQL_PASSWORD}" \
+					--dbhost=mariadb:3306 \
+					--path='/var/www/html' || { echo "Wordpress core config failure"; exit 1; }
 
-	echo "Installing WordPress..."
-	wp core install \
-		--url="${WORDPRESS_URL}" \
-		--title="${WORDPRESS_TITLE}" \
-		--admin_user="${WORDPRESS_ADMIN_USER}" \
-		--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-		--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
-		--allow-root || { echo "Wordpress core install failed"; exit 1; }
+	echo "Installing WordPress"
+	wp core install --url="${WORDPRESS_URL}" \
+					--title="${WORDPRESS_TITLE}" \
+					--admin_user="${WORDPRESS_ADMIN_USER}" \
+					--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+					--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+					--allow-root || { echo "Wordpress core install failed"; exit 1; }
 
 	echo "Creating user..."
 	wp user create "${USER_USERNAME}" "${USER_EMAIL}" \
 		--role=subscriber \
 		--user_pass="${USER_PASSWORD}" \
-		--allow-root|| { echo "Wordpress user create failed"; exit 1; }
+		--allow-root || { echo "Wordpress user create failed"; exit 1; }
 fi
 
 exec php-fpm7.4 -F -R
